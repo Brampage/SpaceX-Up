57b8f18820ea9d86598ebe28d902856b1aa1d83f
Bram Groothedde <bramgroothedde@gmail.com>
2021-06-23 00:34:31 +0200

    Add placeholder layout

---
+++ components/layout.tsx
@@ -0,0 +1,20 @@
+import Head from 'next/head';
+
+import styles from './layout.module.css';
+
+export default function Layout({children, home}: {children: React.ReactNode, home?: boolean}) {
+  return (
+    <div>
+      <Head>
+        <link rel="preload" href="/fonts/B612/B612-Regular.ttf" as="font" crossOrigin="" />
+        <link rel="preload" href="/fonts/B612/B612-Italic.ttf" as="font" crossOrigin="" />
+        <link rel="preload" href="/fonts/B612/B612-Bold.ttf" as="font" crossOrigin="" />
+        <link rel="preload" href="/fonts/B612/B612-BoldItalic.ttf" as="font" crossOrigin="" />
+      </Head>
+      <main>{children}</main>
+      {!home && (
+        <div className={styles.backToHome}></div>
+      )}
+    </div>
+  )
+}
